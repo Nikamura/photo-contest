c9b7c19e1e3fe9dbdeef17c798bf24ea40606515
Karolis Mazukna <k@karolis.dev>
2023-10-06 20:59:31 +0300

    not edge

--- src/routes/contests/[slug]/contribute/+page.server.ts
+++ src/routes/contests/[slug]/contribute/+page.server.ts
@@ -4,12 +4,6 @@ import { GetObjectCommand } from "@aws-sdk/client-s3";
 import type { Actions } from "./$types";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
-export const prerender = false;
-
-export const config = {
-  runtime: "edge",
-};
-
 export const actions = {
   upload: async ({ request }) => {
     const formData = await request.formData();
