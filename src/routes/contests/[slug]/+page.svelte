76f7b7e92d4a7ecf26abe528e0157d52591eeda1
Karolis Mazukna <k@karolis.dev>
2023-10-06 11:41:46 +0300

    run svelte checks before building

--- src/routes/contests/[slug]/+page.svelte
+++ src/routes/contests/[slug]/+page.svelte
@@ -7,7 +7,7 @@
 <div>
   <h1>My Blog</h1>
   <main>
-    <a href="/contests/{data.contest.id}/contribute">Contribute</a>
+    <a href="/contests/{data?.contest?.id}/contribute">Contribute</a>
     <div>hi</div>
   </main>
 </div>
