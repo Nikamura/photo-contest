601fd0bb586a9ee11a563c84c05db858ac8cd9d0
Karolis Mazukna <k@karolis.dev>
2023-10-09 21:30:06 +0300

    fix lint

--- src/routes/contests/[slug]/contribute/+layout.server.ts
+++ src/routes/contests/[slug]/contribute/+layout.server.ts
@@ -1,7 +1,8 @@
 import { redirect } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
 
-export const load: PageServerLoad = async (event) => {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const load: PageServerLoad = async (event: any) => {
   const session = await event.locals.getSession();
   if (!session?.user) throw redirect(303, "/auth");
   return {};
