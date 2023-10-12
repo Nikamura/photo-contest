f47dd448b920b6fc320cc8be470e5eaafc10380e
Karolis Mažukna <k@karolis.dev>
2023-10-12 23:22:45 +0300

    feat: 🎸 use daisyui components (#1)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,21 +4,19 @@
   export let data: PageData;
 </script>
 
-<div class="mx-auto max-w-7xl px-6 py-10 sm:py-14 lg:px-8 lg:py-20">
+<div class="mx-auto max-w-7xl px-6 py-10">
   <h1 class="mb-3 text-2xl">Active contests</h1>
-  <main>
-    <div>
-      <div class="border-b border-gray-200 bg-white px-4 py-5 sm:px-6">
-        {#each data.contests as contest (contest.id)}
-          <div class="bg-white px-4 py-5 text-black sm:px-6">
-            <a href="/contests/{contest.id}">
-              <h2>{contest.name}</h2>
-              <small>Contributions: 0</small>
-              <small>Deadline: XXXX-XX-XX XX:XX</small>
-            </a>
+  <main class="grid grid-cols-1 gap-4 lg:grid-cols-2">
+    {#each data.contests as contest (contest.id)}
+      <div class="card w-96 bg-primary text-primary-content">
+        <div class="card-body">
+          <h2 class="card-title">{contest.name}</h2>
+          <!-- <p>If a dog chews shoes whose shoes does he choose?</p> -->
+          <div class="card-actions justify-end">
+            <a href="/contests/{contest.id}" class="btn">View</a>
           </div>
-        {/each}
+        </div>
       </div>
-    </div>
+    {/each}
   </main>
 </div>
