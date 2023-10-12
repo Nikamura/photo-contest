04f1dbfa5c17906c07be373012ff672bea973cef
Karolis Mazukna <k@karolis.dev>
2023-10-12 23:21:14 +0300

    feat: 🎸 use daisyui components

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,37 +4,29 @@
   import { signIn, signOut } from "@auth/sveltekit/client";
 
   import IconUpload from "virtual:icons/fa/cloud-upload";
-  import { goto } from "$app/navigation";
+  import IconUser from "virtual:icons/fa/user";
 </script>
 
-<header class="bg-white">
-  <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
-    <div class="flex lg:flex-1">
-      <a href="/" class="-m-1.5 p-1.5">
-        <span class="sr-only">CN-</span>
-        <img
-          class="h-8 w-auto"
-          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-          alt=""
-        />
-      </a>
-    </div>
-    <div class="flex flex-1 justify-end">
+<div class="navbar bg-base-100">
+  <div class="flex-1">
+    <a class="btn btn-ghost text-xl normal-case" href="/">🍁 Photo Contest</a>
+  </div>
+  <div class="flex-none">
+    <ul class="menu menu-horizontal px-1">
+      <li><a href="/upload"><IconUpload class="inline" /> Upload</a></li>
       {#if $page.data.session}
-        <span class="mr-1 hidden sm:block">
-          Hey, <strong>{$page.data.session.user?.name ?? "User"}</strong>!
-        </span>
-        <button
-          class="mr-1 rounded px-2 py-1 text-xs font-semibold shadow-sm hover:bg-indigo-100"
-          on:click={() => goto("/upload")}
-        >
-          <IconUpload class="inline" /> Upload
-        </button>
-        <button
-          on:click={() => signOut()}
-          class="rounded bg-indigo-50 px-2 py-1 text-xs font-semibold text-indigo-600 shadow-sm hover:bg-indigo-100"
-          >Sign out</button
-        >
+        <li>
+          <details>
+            <summary
+              ><IconUser class="inline" />
+              <strong>{$page.data.session.user?.name?.split(" ")?.[0] ?? "User"}</strong></summary
+            >
+
+            <ul class="bg-base-100 p-2">
+              <li><button on:click={() => signOut()}>Log out</button></li>
+            </ul>
+          </details>
+        </li>
       {:else}
         <button
           on:click={() => signIn("google")}
@@ -42,10 +34,8 @@
           >Sign in with Google</button
         >
       {/if}
-    </div>
-  </nav>
-</header>
-
-<div class="min-h-screen bg-white text-black dark:bg-slate-800 dark:text-white">
-  <slot />
+    </ul>
+  </div>
 </div>
+
+<slot />
