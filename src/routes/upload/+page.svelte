e110dd7bfda06cb32d6b258a3d878ac709a9dccf
Karolis Mazukna <k@karolis.dev>
2023-10-13 00:23:01 +0300

    feat: 🎸 support multiple file uploads at once

--- src/routes/upload/+page.svelte
+++ src/routes/upload/+page.svelte
@@ -7,12 +7,11 @@
 
   export let form: ActionData;
 
-  async function handleSubmit(event: { currentTarget: EventTarget & HTMLFormElement }) {
-    const actionUrl = event.currentTarget.action;
-    const data = new FormData(event.currentTarget);
+  let uploading = false;
+  let progress = 0;
+  let numberOfUploads = 0;
 
-    const file = data.get("file") as File;
-
+  async function uploadFile(file: File): Promise<string> {
     const resp = await fetch("/api/getUploadUrl", {
       method: "POST",
       body: JSON.stringify({ fileName: file.name }),
@@ -34,8 +33,26 @@
       body: JSON.stringify({ id }),
     });
 
-    data.append("id", id);
+    return id;
+  }
+
+  async function handleSubmit(event: { currentTarget: EventTarget & HTMLFormElement }) {
+    uploading = true;
+    progress = 0;
+    const actionUrl = event.currentTarget.action;
+    const data = new FormData(event.currentTarget);
+
+    const files = data.getAll("files") as File[];
+    numberOfUploads = files.length;
+
+    for (const file of files) {
+      const id = await uploadFile(file);
+      data.append("ids", id);
+      progress++;
+    }
+
     data.delete("file");
+    data.delete("files");
 
     const response = await fetch(actionUrl, {
       method: "POST",
@@ -49,22 +66,49 @@
     }
 
     applyAction(result);
+    uploading = false;
   }
 </script>
 
-<div>Contribute</div>
+<div class="mx-auto max-w-7xl px-6 py-10">
+  <h1 class="mb-3 text-2xl">Upload your photos</h1>
 
-<form
-  method="POST"
-  action="?/upload"
-  enctype="multipart/form-data"
-  on:submit|preventDefault={handleSubmit}
->
-  <input type="file" name="file" accept="image/*" required />
-  <button type="submit">Submit</button>
+  <form
+    method="POST"
+    action="?/upload"
+    enctype="multipart/form-data"
+    on:submit|preventDefault={handleSubmit}
+  >
+    <div class="form-control w-full max-w-xs">
+      <label class="label" for="files">
+        <span class="label-text">Pick your photos</span>
+      </label>
+      <input
+        type="file"
+        name="files"
+        accept="image/*"
+        required
+        class="file-input file-input-bordered w-full max-w-xs"
+        multiple
+      />
+    </div>
 
-  {#if form}
-    <!-- svelte-ignore a11y-missing-attribute -->
-    <img src={form.url} />
-  {/if}
-</form>
+    <button type="submit" disabled={uploading} class="btn btn-primary mt-2">
+      {#if uploading}
+        <span class="loading loading-xs"></span>
+        Uploading {progress}/{numberOfUploads}...
+      {:else}
+        Upload
+      {/if}</button
+    >
+
+    {#if form}
+      <h2 class="mt-5 text-xl">Preview</h2>
+      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        {#each form.urls as url}
+          <div class="mb-1 sm:mr-1"><img src={url} alt="user's upload thumbnail" /></div>
+        {/each}
+      </div>
+    {/if}
+  </form>
+</div>
