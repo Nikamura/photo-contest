1c150dfd3a77f3cf54bb1de19b06e83c57ff856e
Karolis Mazukna <k@karolis.dev>
2023-10-14 11:37:25 +0300

    fixes

--- src/lib/trigger.ts
+++ src/lib/trigger.ts
@@ -1,9 +1,9 @@
 import { TriggerClient } from "@trigger.dev/sdk";
-import { TRIGGER_API_URL } from "$env/static/private";
+import { TRIGGER_API_KEY } from "$env/static/private";
 
 const client = new TriggerClient({
   id: "photo-contest",
-  apiUrl: TRIGGER_API_URL,
+  apiKey: TRIGGER_API_KEY,
 });
 
 export default client;
