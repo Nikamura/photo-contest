ac45e09718461c23fca840e3a2d22e1cba53330b
Karolis Mazukna <k@karolis.dev>
2024-02-04 18:52:23 +0200

    Remove Trigger SDK and related files

--- src/lib/trigger.ts
+++
@@ -1,9 +0,0 @@
-import { TriggerClient } from "@trigger.dev/sdk";
-import { TRIGGER_API_KEY } from "$env/static/private";
-
-const client = new TriggerClient({
-  id: "photo-contest",
-  apiKey: TRIGGER_API_KEY,
-});
-
-export default client;
