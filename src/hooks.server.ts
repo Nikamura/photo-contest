fc360cbdde06a33888e2bf91b018cace78e03929
Karolis Mazukna <k@karolis.dev>
2024-03-15 13:33:48 +0200

    Fix SvelteKitAuth handle import and remove unused code

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,9 +3,8 @@ import Google from "@auth/core/providers/google";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { AUTH_GOOGLE_CLIENT_ID, AUTH_GOOGLE_CLIENT_SECRET, AUTH_SECRET } from "$env/static/private";
 import { prismaClient } from "$lib/prisma";
-import type { Handle } from "@sveltejs/kit";
 
-const { handle: handlerFunc, signIn, signOut } = SvelteKitAuth(async () => {
+const { handle, signIn, signOut } = SvelteKitAuth(async () => {
   const authOptions = {
     adapter: PrismaAdapter(prismaClient),
     providers: [
@@ -28,21 +27,5 @@ const { handle: handlerFunc, signIn, signOut } = SvelteKitAuth(async () => {
   } satisfies SvelteKitAuthConfig;
   return authOptions;
 });
-
-// Fix from https://github.com/nextauthjs/next-auth/issues/6451#issuecomment-1399793425
-// So it works via reverse proxy :/
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const handle: Handle = ({ event, resolve }: any) => {
-  // event.url.protocol = "http:";
-
-  // const symbol = Object.getOwnPropertySymbols(event.request)[1];
-
-  // event.request[symbol].url.protocol = "http:";
-  // for (let i = 0; i < event.request[symbol].urlList.length; i++) {
-  //   event.request[symbol].urlList[i].protocol = "http:";
-  // }
-
-  return handlerFunc({ event, resolve });
-};
 
 export { handle, signIn, signOut };
